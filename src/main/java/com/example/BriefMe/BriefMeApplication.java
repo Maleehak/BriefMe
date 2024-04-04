94b91906783eefece713704a4c3593d36b1a19a2
Maleeha Khalid <maleeha.khalid@confiz.com>
2024-04-04 23:48:54 +0500

    add speech to text converter

--- src/main/java/com/example/BriefMe/BriefMeApplication.java
+++ src/main/java/com/example/BriefMe/BriefMeApplication.java
@@ -1,18 +1,12 @@
 package com.example.BriefMe;
 
-import com.example.BriefMe.service.client.AudioExtractor;
-import com.example.BriefMe.service.impl.Mp4AudioExtractorImpl;
-import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BriefMeApplication {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) {
 		SpringApplication.run(BriefMeApplication.class, args);
-		AudioExtractor audioExtractor = new Mp4AudioExtractorImpl();
-		audioExtractor.extractAudio("ESP-CMS-KT.mp4");
-
 	}
 }
--- src/main/java/com/example/BriefMe/service/impl/AudioToTextConverterImpl.java
+++ src/main/java/com/example/BriefMe/service/impl/AudioToTextConverterImpl.java
@@ -8,34 +8,69 @@ import com.google.cloud.speech.v1.RecognizeResponse;
 import com.google.cloud.speech.v1.SpeechClient;
 import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
 import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class AudioToTextConverterImpl implements AudioToTextConverter {
 
-    public void covertAudioToText(String audioFile) throws IOException {
-        // Instantiates a client
+    public String covertAudioToText(String audioFile) throws IOException {
+
         try (SpeechClient speechClient = SpeechClient.create()) {
+
+            log.info("Loading the audio file into memory...");
+
+            Path path = Paths.get(audioFile);
+            byte[] data = Files.readAllBytes(path);
+            log.info("Audio file loaded...");
+
+            int chunkSize = 2097152; // 2MB
+            int numChunks = (int) Math.ceil((double) data.length / chunkSize);
+
+            log.info("Audio file is divided into {} chunks", numChunks);
 
             // Builds the sync recognize request
             RecognitionConfig config =
                     RecognitionConfig.newBuilder()
-                            .setEncoding(AudioEncoding.LINEAR16)
+                            .setEncoding(AudioEncoding.MP3)
                             .setSampleRateHertz(16000)
                             .setLanguageCode("en-US")
+                            .setEnableAutomaticPunctuation(true)
                             .build();
-            RecognitionAudio audio = RecognitionAudio.newBuilder().setUri(audioFile).build();
 
-            // Performs speech recognition on the audio file
-            RecognizeResponse response = speechClient.recognize(config, audio);
-            List<SpeechRecognitionResult> results = response.getResultsList();
+            String completeTranscript = "";
 
-            for (SpeechRecognitionResult result : results) {
-                // There can be several alternative transcripts for a given chunk of speech. Just use the
-                // first (most likely) one here.
-                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-                System.out.printf("Transcription: %s%n", alternative.getTranscript());
+            for (int i = 0; i < numChunks; i++) {
+
+                // Extract the current chunk
+                int start = i * chunkSize;
+                int end = Math.min((i + 1) * chunkSize, data.length);
+                byte[] chunkData = new byte[end - start];
+                System.arraycopy(data, start, chunkData, 0, end - start);
+                ByteString chunkBytes = ByteString.copyFrom(chunkData);
+
+                RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(chunkBytes).build();
+
+                log.info("Performing speech to text conversion on the chunk {}", i);
+                RecognizeResponse response = speechClient.recognize(config, audio);
+                List<SpeechRecognitionResult> results = response.getResultsList();
+
+                int j = 0;
+                for (SpeechRecognitionResult result : results) {
+                    SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                    log.info(" Chunk {} - Transcription {} : {}",i, j, alternative.getTranscript());
+                    completeTranscript = completeTranscript + alternative.getTranscript();
+                    j++;
+                }
             }
+
+            log.info("Audio conversion to text completed. Transcript {}", completeTranscript);
+            return completeTranscript;
         }
     }
 }
