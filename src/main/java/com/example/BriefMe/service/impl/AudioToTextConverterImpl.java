872c6177f518f0c0458231ab43bfb7acf7a88d21
Maleehak <maleehakhalid604@gmail.com>
2024-04-08 23:58:12 +0500

    improve performance of audio to text

--- src/main/java/com/example/BriefMe/service/impl/AudioToTextConverterImpl.java
+++ src/main/java/com/example/BriefMe/service/impl/AudioToTextConverterImpl.java
@@ -1,19 +1,15 @@
 package com.example.BriefMe.service.impl;
 
 import com.example.BriefMe.service.client.AudioToTextConverter;
-import com.google.cloud.speech.v1.RecognitionAudio;
-import com.google.cloud.speech.v1.RecognitionConfig;
-import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
-import com.google.cloud.speech.v1.RecognizeResponse;
+import com.example.BriefMe.service.util.SpeechRecognitionTask;
 import com.google.cloud.speech.v1.SpeechClient;
-import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
-import com.google.cloud.speech.v1.SpeechRecognitionResult;
-import com.google.protobuf.ByteString;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -34,45 +30,30 @@ public class AudioToTextConverterImpl implements AudioToTextConverter {
 
             log.info("Audio file is divided into {} chunks", numChunks);
 
-            // Builds the sync recognize request
-            RecognitionConfig config =
-                    RecognitionConfig.newBuilder()
-                            .setEncoding(AudioEncoding.MP3)
-                            .setSampleRateHertz(16000)
-                            .setLanguageCode("en-US")
-                            .setEnableAutomaticPunctuation(true)
-                            .build();
+            StringBuilder completeTranscript = new StringBuilder();
 
-            String completeTranscript = "";
+            CountDownLatch latch = new CountDownLatch(numChunks);
 
+            ExecutorService executor = Executors.newFixedThreadPool(numChunks);
             for (int i = 0; i < numChunks; i++) {
-
-                // Extract the current chunk
-                int start = i * chunkSize;
-                int end = Math.min((i + 1) * chunkSize, data.length);
-                byte[] chunkData = new byte[end - start];
-                System.arraycopy(data, start, chunkData, 0, end - start);
-                ByteString chunkBytes = ByteString.copyFrom(chunkData);
-
-                RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(chunkBytes).build();
-
-                log.info("Performing speech to text conversion on the chunk {}", i);
-                RecognizeResponse response = speechClient.recognize(config, audio);
-                List<SpeechRecognitionResult> results = response.getResultsList();
-
-                int j = 0;
-                for (SpeechRecognitionResult result : results) {
-                    SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-                    log.info(" Chunk {} - Transcription {} : {}",i, j, alternative.getTranscript());
-                    completeTranscript = completeTranscript + alternative.getTranscript();
-                    j++;
-                }
+                executor.submit(new SpeechRecognitionTask(i, latch, speechClient, data, chunkSize));
             }
 
-            // TODO: Delete the audio file after the text has been read
+            try {
+                // Wait for all threads to complete
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            for (int i = 0; i < numChunks; i++) {
+                completeTranscript.append(SpeechRecognitionTask.resultQueue.poll());
+            }
+
+            executor.shutdown();
 
             log.info("Audio conversion to text completed. Transcript: {}", completeTranscript);
-            return completeTranscript;
+            return completeTranscript.toString();
         }
     }
 }
---
+++ src/main/java/com/example/BriefMe/service/util/SpeechRecognitionTask.java
@@ -0,0 +1,72 @@
+package com.example.BriefMe.service.util;
+
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SpeechRecognitionTask implements Runnable{
+    private final int taskId;
+    private final int chunkSize;
+    private final CountDownLatch latch;
+    private final byte[] data;
+    private final SpeechClient speechClient;
+    public static BlockingQueue<String> resultQueue = new LinkedBlockingQueue<>();
+
+    RecognitionConfig config =
+            RecognitionConfig.newBuilder()
+                    .setEncoding(AudioEncoding.MP3)
+                    .setSampleRateHertz(16000)
+                    .setLanguageCode("en-US")
+                    .setEnableAutomaticPunctuation(true)
+                    .build();
+
+    public SpeechRecognitionTask(int taskId, CountDownLatch latch, SpeechClient speechClient,
+            byte[] data, int chunkSize) {
+        this.taskId = taskId;
+        this.latch = latch;
+        this.data = data;
+        this.speechClient = speechClient;
+        this.chunkSize = chunkSize;
+    }
+
+    @Override
+    public void run() {
+        // Extract the chunk
+        int start = this.taskId * this.chunkSize;
+        int end = Math.min((taskId + 1) * this.chunkSize, data.length);
+
+        byte[] chunkData = new byte[end - start];
+        System.arraycopy(data, start, chunkData, 0, end - start);
+        ByteString chunkBytes = ByteString.copyFrom(chunkData);
+
+        RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(chunkBytes).build();
+
+        log.info("Performing speech to text conversion on the chunk {}", this.taskId);
+        RecognizeResponse response = speechClient.recognize(config, audio);
+        List<SpeechRecognitionResult> results = response.getResultsList();
+
+        int j = 0;
+        StringBuilder transcript = new StringBuilder();
+        for (SpeechRecognitionResult result : results) {
+            SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+            log.info(" Chunk {} - Transcription {} : {}", this.taskId , j, alternative.getTranscript());
+            transcript.append(alternative.getTranscript());
+            j++;
+        }
+
+        resultQueue.add(transcript.toString());
+
+        this.latch.countDown();
+    }
+}
